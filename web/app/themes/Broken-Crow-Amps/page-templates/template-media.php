2fe65d852ff4c2dbde9943b7b1ea58e467eca43a
Josh Tipton <josh@blue-bot.com>
2016-07-07 19:50:24 -0400

    Media Page Features Addition

--- web/app/themes/Broken-Crow-Amps/page-templates/template-media.php
+++ web/app/themes/Broken-Crow-Amps/page-templates/template-media.php
@@ -4,11 +4,35 @@ Template Name: Media Page
 */
 get_header(); ?>
 
-		<div class="amps-main row">
-			<h1><?php the_title(); ?> <span style="font-weight: normal;">(Coming Soon)</span></h1>
-
-
+<div class="amps-main">
+	<div class="row collapse">
+		<div class="large-12 columns">
+			<h1><?php the_title(); ?></h1>
 		</div>
+	</div>
+
+	<div class="row">
+		<div class="large-10 large-centered columns">
+			<?php the_field('featured_video'); ?>
+		</div>
+
+		<?php if( have_rows('other_videos') ): ?>
+				<div class="other-vids-wrap clearfix">
+				    <?php while ( have_rows('other_videos') ) : the_row(); ?>
+
+				    	<div class="single-vid large-4 columns">
+							<?php the_sub_field('video_column'); ?>
+
+							<?php the_sub_field('video_title'); ?>
+						</div>
+
+				    <?php endwhile; ?>
+				</div>
+			<?php else : ?>
+		<?php endif; ?>
+
+	</div>
+</div>
 
 
 <?php get_footer();
